1f922937df5f851b1cd62f7f4fadd117c4586ed5
Zied SAID <32530016+ziedsaidd@users.noreply.github.com>
2020-10-14 14:30:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -229,6 +229,6 @@ def update_covid_timeseries(hoverData):
 
 
 if __name__ == '__main__':
-    #server.run(debug=True)
-    app.run_server(debug=True)
+    server.run(debug=True)
+    #app.run_server(debug=True)
     #server.run(host = "0.0.0.0", port= 5000)
